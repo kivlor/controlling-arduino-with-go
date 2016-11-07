236762ed4d07a87d70e9cbbcec112e0cca5a0d97
Andrew Gallagher <kivlor@gmail.com>
2016-11-08 00:08:37 +1000

    demos done

--- demo-one.go
+++ demo-one.go
@@ -18,27 +18,32 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer s.Close()
 
-	// write an r,g,b string to the serial pot
-	n, err := s.Write([]byte("255,0,0\n"))
+	// write an r,g,b string to the serial port
+	_, err = s.Write([]byte("255,0,0\n"))
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// read any data that may come back from the serial port
-	buf := make([]byte, 128)
-	n, err = s.Read(buf)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// print the data
-	log.Printf("%q", buf[:n])
 
 	// delay for a few seconds so we can see the color
-	time.Sleep(time.Second * 2)
+	time.Sleep(time.Second * 1)
 
-	// no need to close the serial, taum/serial does that for us
-	// bounce 🏀
-	return
+	// write another r,g,b string to the serial port
+	_, err = s.Write([]byte("0,255,0\n"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// delay for a few seconds so we can see the color
+	time.Sleep(time.Second * 1)
+
+	// write another r,g,b string to the serial port
+	_, err = s.Write([]byte("0,0,255\n"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// delay for a few seconds so we can see the color
+	time.Sleep(time.Second * 1)
 }
---
+++ demo-two.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/tarm/serial"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var s *serial.Port
+
+func main() {
+	// create a config struct
+	c := &serial.Config{
+		Name: "/dev/tty.usbmodem1441",
+		Baud: 9600,
+	}
+
+	// open the serial port using the config
+	var err error
+	s, err = serial.OpenPort(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer s.Close()
+
+	// crate a basic web server...
+	http.HandleFunc("/", root)
+	http.HandleFunc("/change", change)
+	http.ListenAndServe(":8080", nil)
+}
+
+func root(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.New("home").Parse(`
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>color picker</title>
+    <style>
+      * { box-sizing: border-box; }
+      div#colors { margin: 20px auto; width: 600px; overflow: hidden; }
+      div#colors div { display: inline; float: left; width: 200px; height: 200px; border: 10px solid #fff; cursor: pointer; }
+    </style>
+  </head>
+  <body>
+    <div id="colors">
+      <div style="background: rgb(255, 0, 0);" onclick="sendColor(255, 0, 0)"></div>
+      <div style="background: rgb(255, 255, 0);" onclick="sendColor(255, 255, 0)"></div>
+      <div style="background: rgb(0, 255, 0);" onclick="sendColor(0, 255, 0)"></div>
+      <div style="background: rgb(0, 255, 255);" onclick="sendColor(0, 255, 255)"></div>
+      <div style="background: rgb(0, 0, 255);" onclick="sendColor(0, 0, 255)"></div>
+      <div style="background: rgb(255, 255, 255);" onclick="sendColor(255, 255, 255)"></div>
+    </div>
+    <script type="text/javascript">
+      function sendColor(red, green, blue) {
+        var request = new XMLHttpRequest();
+        request.open('POST', '/change', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({ r: +red, g: +green, b: +blue }));
+      }
+    </script>
+  </body>
+</html>
+`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tmpl.Execute(w, nil)
+}
+
+func change(w http.ResponseWriter, r *http.Request) {
+	// create color struct
+	var c struct {
+		Red   int `json:"r"`
+		Green int `json:"g"`
+		Blue  int `json:"b"`
+	}
+
+	// decode json data
+	err := json.NewDecoder(r.Body).Decode(&c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// write an r,g,b string to the serial port
+	_, err = s.Write([]byte(fmt.Sprintf("%v,%v,%v\n", c.Red, c.Green, c.Blue)))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Write([]byte("OK"))
+}
